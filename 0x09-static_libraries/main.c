450e7cd3e1a3d19e358ea3877fb9e9c9c867840c
Mpho94 <mashegompho14@gmail.com>
2023-07-12 14:41:46 -0700

    static-lib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,10 @@
-{
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+#include <stdio.h>
+
+void _puts(const char* str) {
+    printf("%s\n", str);
+}
+
+int main() {
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return 0;
 }
